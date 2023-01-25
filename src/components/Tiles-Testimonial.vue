14ed4cfc657c31195af31b8ef443e24dcb3f3055
MohammadFahad8 <mohammadfahad.work@gmail.com>
2023-01-26 00:11:26 +0500

    tiles section

---
+++ src/components/Tiles-Testimonial.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Tiles from "./Tiles.vue"
+</script>
+<template>
+  <div class="container mt-4 justify-content-center text-center">
+    <div class="border-0 card justify-content-center text-center">
+      <div class="border-0 card-content justify-content-center text-center">
+        <img src="../assets/tiles-jumbotron.png" alt="">
+      </div>
+    </div>
+    <Tiles></Tiles>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import jumbotron from "../components/Jumbotron.vue";
+import TilesTestimonial from "../components/Tiles-Testimonial.vue";
 
 </script>
 
 <template>
   <main>
 <jumbotron></jumbotron>
+  <TilesTestimonial></TilesTestimonial>
   </main>
 </template>
